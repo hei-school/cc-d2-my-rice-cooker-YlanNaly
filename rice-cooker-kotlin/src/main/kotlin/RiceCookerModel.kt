19fdb236bb323ccf5fc14b4be531420ac4d3287c
YlanNaly <hei.nalisoa@gmail.com>
2023-12-10 12:55:31 +0300

    feat: initialize functionnality

---
+++ rice-cooker-kotlin/src/main/kotlin/RiceCookerModel.kt
@@ -0,0 +1,25 @@
+class RiceCookerModel : IRiceCooker {
+    override fun cook(minutes: String) {
+        TODO("Not yet implemented")
+    }
+	
+    override fun boil(minutes: String) {
+        TODO("Not yet implemented")
+    }
+	
+    override fun powerOn(power: Boolean) {
+        TODO("Not yet implemented")
+    }
+	
+    override fun powerOff(power: Boolean) {
+        TODO("Not yet implemented")
+    }
+	
+    override fun keepWarm(status: Boolean) {
+        TODO("Not yet implemented")
+    }
+	
+    override fun stopWarm(status: Boolean) {
+        TODO("Not yet implemented")
+    }
+}
